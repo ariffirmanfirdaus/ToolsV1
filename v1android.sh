af16846e633a23143d7ffad762bd86ba0ec64dcb
ariffirmanfirdaus <51688579+ariffirmanfirdaus@users.noreply.github.com>
2020-01-27 12:44:27 +0700

    Add files via upload

---
+++ v1android.sh
@@ -0,0 +1,237 @@
+clear
+blue='\33[34;1m'
+green='\33[32;1m'
+purple='\33[35;1m'
+cyan='\33[36;1m'
+red ='\33[31;1m'
+white='\33[37;1m'
+yellow:'\33[33;1m'
+echo
+toilet -f big -F gay By ARIF.F.F
+echo $green "wellcom To my Script"
+echo
+sleep 2
+echo $white "Silakan Pilih : "
+echo 
+echo $red"01.) Stabilkan Jaringan"
+echo $red"02.) Daftar"
+echo $red"03.) login"
+echo $red"00.) keluar!!"
+echo
+echo $white
+read -p "pilih manah : " bro
+
+if [ $bro = 01 ] || [ $bro = 01 ]
+then
+clear
+echo
+toilet -f big -F gay "Load..."
+echo $yellow "Stabilkan Ping"
+sleep 2
+ping -s1000 1.1.1.1
+fi
+
+
+if [ $bro = 00 ] || [ $bro = 00 ]
+then
+clear
+figlet "Bye"
+echo $cyan "To Be Contuned"
+exit
+fi
+
+
+if [ $bro = 02 ] || [ $bro = 02 ]
+then
+clear
+echo
+read -p "imail : "
+sleep 2
+echo $blue "sukses"
+echo
+read -p "Pass : "
+echo
+read -p "Confirmasi Pass : "
+echo $Bule
+toilet -f big -F gay "Sukses"
+sleep 3
+clear
+echo $yellow "Silakan pilih : "
+echo $green "1.) Download Script 400 "
+echo $green "2.) Download Script 300 "
+echo $green "3.) Download Script script-deface-creatorA "
+echo $green "4.) Download  Script 1000 "
+echo $green "99.) Keluar "
+echo $yellow
+read -p "Pilih Manah : "bro
+
+if [ $bro = 1 ] || [ $bro = 1 ]
+then
+clear
+echo $purple
+toilet -f big -F gay "Memproses"
+echo
+sleep 3
+clear
+echo $white "Tunggu"
+echo
+sleep 3
+clear
+git clone https://github.com/tuanb4dut/toolsinstallerv4
+fi
+
+
+if [ $bro = 2 ] || [ $bro = 2 ]
+then
+clear
+echo $purple
+toilet -f big -F gay "Memproses"
+echo
+sleep 3
+clear
+echo $white "Tunggu"
+echo
+sleep 3
+clear
+git clone https://github.com/tuanb4dut/toolsinstallerv3
+fi
+
+
+if [ $bro = 3 ] || [ $bro = 3 ]
+then
+clear
+echo $purple
+toilet -f big -F gay "Memproses"
+echo
+sleep 3
+clear
+echo $white "Tunggu"
+echo
+sleep 3
+clear
+git clone https://github.com/Ubaii/script-deface
+fi
+
+
+if [ $bro = 4 ] || [ $bro = 4 ]
+then
+clear
+echo $purple
+toilet -f big -F gay "Memproses"
+echo
+sleep 3
+clear
+echo $white "Tunggu"
+echo
+sleep 3
+clear
+git clone
+https://github.com/ariffirmanfirdaus/v1Coy
+fi
+
+
+if [ $bro = 99 ] || [ $bro = 99 ]
+then
+clear
+figlet "Bye"
+echo $cyan "To Be Contuned"
+exit
+fi
+
+
+if [ $bro = 03 ] || [ $bro = 03 ]
+then
+clear
+echo $yellow
+toilet -f big -F gay "LOGIN..."
+echo $white
+read -p "imail : "
+sleep 1
+echo $blue "sukses"
+echo
+read -p "pass : "
+sleep 1
+echo $blue "sukses"
+sleep 3
+clear
+echo $yellow "Silakan pilih : "
+echo $green "1.) Download Script 400 "
+echo $green "2.) Download Script 300 "
+echo $green "3.) Download Script script-deface-creatorA "
+echo $green "4.) Download  Script 1000 "
+echo $green "99.) Keluar "
+echo $yellow
+read -p "Pilih Manah : "bro
+
+if [ $bro = 1 ] || [ $bro = 1 ]
+then
+clear
+echo $purple
+toilet -f big -F gay "Memproses"
+echo
+sleep 3
+clear
+echo $white "Tunggu"
+echo
+sleep 3
+clear
+git clone https://github.com/tuanb4dut/toolsinstallerv4
+fi
+
+
+if [ $bro = 2 ] || [ $bro = 2 ]
+then
+clear
+echo $purple
+toilet -f big -F gay "Memproses"
+echo
+sleep 3
+clear
+echo $white "Tunggu"
+echo
+sleep 3
+clear
+git clone https://github.com/tuanb4dut/toolsinstallerv3
+fi
+
+
+if [ $bro = 3 ] || [ $bro = 3 ]
+then
+clear
+echo $purple
+toilet -f big -F gay "Memproses"
+echo
+sleep 3
+clear
+echo $white "Tunggu"
+echo
+sleep 3
+clear
+git clone https://github.com/Ubaii/script-deface
+fi
+
+
+if [ $bro = 4 ] || [ $bro = 4 ]
+then
+clear
+echo $purple
+toilet -f big -F gay "Memproses"
+echo
+sleep 3
+clear
+echo $white "Tunggu"
+echo
+sleep 3
+clear
+git clone
+https://github.com/ariffirmanfirdaus/v1Coy
+fi
+
+
+if [ $bro = 99 ] || [ $bro = 99 ]
+then
+clear
+figlet "Bye"
+echo $cyan "To Be Contuned"
+exit
+fi
